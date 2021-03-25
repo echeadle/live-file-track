480adadd5098396d8160ced9455b92b7e95bb682
Edward Cheadle <edward.cheadle8@gmail.com>
2021-03-24 18:54:13 -0600

    Change paramerter to reflecdt I want the full path to the DB

--- filechanges.py
+++ filechanges.py
@@ -1,11 +1,13 @@
 import sqlite3
+import sys
 
 
 # Write a function that can create an SQLite instance.
 
-def create_file_tracking_db(name):
+def create_file_tracking_db(full_path_to_name):
     """Create File Tracking Database"""
-    return sqlite3.connect(name)
+    return sqlite3.connect(full_path_to_name)
+)
 
 
 # Write a function that can query the master database 
