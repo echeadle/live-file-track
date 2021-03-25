0571e1f6d05b082740aa2665740db4aa1c0eba71
Edward Cheadle <edward.cheadle8@gmail.com>
2021-03-24 18:32:04 -0600

    Create sqllite db

--- filechanges.py
+++ filechanges.py
@@ -1,2 +1,34 @@
 import sqlite3
 
+
+# Write a function that can create an SQLite instance.
+
+def create_file_tracking_db(name):
+    """Create File Tracking Database"""
+    return sqlite3.connect(name)
+
+
+# Write a function that can query the master database 
+# for the SQLite instance created.
+
+
+def main():
+    db_name = 'first.db'
+    conn = create_file_tracking_db(db_name)
+   
+
+    cursor = conn.cursor()
+
+    cursor.execute("create table people (id integer primary key, name text, count integer)")
+    cursor.execute("insert into people (name, count) values ('Bob', 1)")
+
+    conn.commit()
+
+    result = cursor.execute("select * from people")
+    print(result.fetchall())
+
+    conn.close()
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
